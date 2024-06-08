2b63a6e3ee5bea2b702742e89e3d98c937f21c22
Andrey Grytsenko <63291871+GrytsenkoAndrey@users.noreply.github.com>
2024-06-08 11:12:19 +0300

    Update code.js

--- code.js
+++ code.js
@@ -2,3 +2,16 @@
 //                 good for simple functions that you use only once
 //                 (parameters) => some code
 
+// 1
+// function hello() {
+//   console.log("hello");
+// }
+// 2
+// const hello = function() {
+//   console.log('hello');
+// }
+// hello();
+// the same but
+const hello = () => console.log('hello');
+
+hello();
